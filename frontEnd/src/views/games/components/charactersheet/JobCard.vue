6f16f639243aad99113444d2605fa9bc503495b3
송근웅 <rmsdndchlrh@naver.com>
2024-07-25 11:00:56 +0900

    FE:Voice Chat(23)
    음성채팅 기능 구현(on/off, 미완성), 캐릭터시트 페이지 구현 중

--- frontEnd/src/views/games/components/charactersheet/JobCard.vue
+++ frontEnd/src/views/games/components/charactersheet/JobCard.vue
@@ -1,16 +1,44 @@
 <template>
-    <div class="job-card">
-      <!-- 직업별 카드 시트 내용 -->
-      <h2>Job Card</h2>
-    </div>
-  </template>
-  
-  <style scoped>
-  .job-card-sheet {
-    background-color: #444;
-    color: white;
-    padding: 10px;
-    border-radius: 5px;
+  <div class="job-card">
+    <img :src="image" alt="Job Image" class="job-image" />
+    <h3>{{ title }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    image: {
+      type: String,
+      required: true
+    }
   }
-  </style>
-  
+};
+</script>
+
+<style scoped>
+.job-card {
+  background-color: #444;
+  color: white;
+  padding: 10px;
+  border-radius: 5px;
+  text-align: center;
+  width: 330px; /* 카드의 너비를 적절히 조정 */
+  margin: 10px; /* 카드 간격을 조정 */
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.job-image {
+  width: 100px;
+  height: 150px;
+  object-fit: cover;
+  border-radius: 50%;
+  margin-bottom: 10px; /* 이미지와 텍스트 사이의 간격을 추가 */
+}
+</style>

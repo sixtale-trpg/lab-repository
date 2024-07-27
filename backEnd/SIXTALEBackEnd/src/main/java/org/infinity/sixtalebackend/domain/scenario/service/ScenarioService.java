8a7f86114246be3f03bf33dd431454a36e3c50ef
namgyeongmin <skarudals27@naver.com>
2024-07-27 19:48:52 +0900

    Feat-BE : Read ScenarioInfo (시나리오 상세 조회) (#15)
    - 시나리오 상세 조회
    - JPA PAgeable 직렬화 경고 이슈로 ScenarioListResponseDto에 담아서 보냄

--- backEnd/SIXTALEBackEnd/src/main/java/org/infinity/sixtalebackend/domain/scenario/service/ScenarioService.java
+++ backEnd/SIXTALEBackEnd/src/main/java/org/infinity/sixtalebackend/domain/scenario/service/ScenarioService.java
@@ -1,6 +1,7 @@
 package org.infinity.sixtalebackend.domain.scenario.service;
 
 import org.infinity.sixtalebackend.domain.member.dto.MemberResponseDto;
+import org.infinity.sixtalebackend.domain.scenario.dto.ScenarioListResponseDto;
 import org.infinity.sixtalebackend.domain.scenario.dto.ScenarioResponseDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,7 +10,7 @@ import java.util.List;
 
 public interface ScenarioService {
 
-    Page<ScenarioResponseDto> getScenarioList(Long memberID, Long genreID, String title, Pageable diaryPageable);
+    ScenarioListResponseDto getScenarioList(Long memberID, Long genreID, String title, Pageable diaryPageable);
 
     ScenarioResponseDto getScenarioInfo(Long scenarioID, Long memberID);
 }

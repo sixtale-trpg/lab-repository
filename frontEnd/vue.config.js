a629a7fe093aee8f05ab6e4f05cadfd005aaffa4
정수현 <jshinkor00@naver.com>
2024-07-28 01:12:06 +0900

    Chore: update frontEnd/vue.config.js

--- frontEnd/vue.config.js
+++ frontEnd/vue.config.js
@@ -30,7 +30,7 @@ module.exports = {
     open: true,
     proxy: {
       '/api/v1': {
-        target: 'http://localhost:8080/'
+        target: 'http://i11d108.p.ssafy.io/:8080/'
       }
     },
     historyApiFallback: true,

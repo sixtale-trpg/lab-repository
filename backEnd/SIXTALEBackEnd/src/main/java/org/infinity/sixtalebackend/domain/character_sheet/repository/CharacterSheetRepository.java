a3f6887fa9c60bd34bdeb0dd4045d19f5d9c160f
안치숙 <songweddin@naver.com>
2024-07-29 02:51:58 +0900

    Feat-BE: create characterSheet & fix entity (#32)
    캐릭터 시트 작성 api 구현
    > entity 변화
    1. Character_stat의 stat_weight 필드 타입을 boolean에서 Integer로 변경
    - 직접: stat_weight 타입 Integer로 변경
    2. CharacterEquipment의 weight 필드 이름 변경(statWeight였음), 타입 boolean에서 Integer로 변경
    - jpa 자동 변경: weight 필드 추가
    - 직접: statWeight 필드 제거
    3. CharacterEquipment의 Equipment 필드 참조를 ScenarioEquipment로 변경(equipment였었음)
     - jpa 자동 변경: equipment필드가 scenarioEquipment를 참조하도록 하는 외래키 추가
     - 직접: equipment필드에 equipment를 참조하는 외래키를 삭제
    4. job 테이블의 description이 @Lob만 붙이니까 tinytext로 인식
     -   @Lob     @Column(columnDefinition = TEXT) 이렇게 해야 해결
     => 모든 @Lob이 아마 tinytext로 지정되었을 것, @Lob과 @Column(columnDefinition = TEXT)함께 써야 text로 인식
    5. actionOption에서 diceType의 nullable=false삭제
     - 직접: dice_type null 속성 허용 바꿈
    6. scenario_equipment에서 시나리오 아이디 필드 추가

--- backEnd/SIXTALEBackEnd/src/main/java/org/infinity/sixtalebackend/domain/character_sheet/repository/CharacterSheetRepository.java
+++ backEnd/SIXTALEBackEnd/src/main/java/org/infinity/sixtalebackend/domain/character_sheet/repository/CharacterSheetRepository.java
@@ -1,2 +1,11 @@
-package org.infinity.sixtalebackend.domain.character_sheet.repository;public class CharacterSheetRepository {
+package org.infinity.sixtalebackend.domain.character_sheet.repository;
+
+import org.infinity.sixtalebackend.domain.character_sheet.domain.CharacterSheet;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+@Transactional(readOnly = true)
+public interface CharacterSheetRepository extends JpaRepository<CharacterSheet, Long> {
 }

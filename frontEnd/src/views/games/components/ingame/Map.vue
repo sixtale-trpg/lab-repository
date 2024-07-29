f42bf845a7de739e952254583445e19b342d3f91
안치숙 <songweddin@naver.com>
2024-07-29 14:38:35 +0900

    Revert "Merge branch '8-feat-fe-roll-dice' into 'feature-front'"
    This reverts merge request !72

--- frontEnd/src/views/games/components/ingame/Map.vue
+++ frontEnd/src/views/games/components/ingame/Map.vue
@@ -1,9 +1,4 @@
 <template>
-  <div class="map-section-container">
-    <img src="@/assets/images/maps/map1.png" alt="Map" class="map-image" />
-    <div ref="rendererContainer" class="renderer-container">
-  </div>
-
   <div class="map-section-container" @dragover.prevent @drop="onDrop">
     <img :src="mapImage" alt="Map" class="map-image" />
     <div 
@@ -26,31 +21,7 @@
 </template>
 
 <script setup>
-
 import { ref, computed, onMounted, onUnmounted } from 'vue';
-
-import ThreeJSManager from '@/common/lib/ThreeJSManager';
-import eventBus from '@/common/lib/eventBus.js'; // Ensure this path is correct
-
-const rendererContainer = ref(null);
-let threeJSManager = null;
-
-const handleRollDice = (diceTypesToRoll) => {
-  console.log('Received roll-dice event with:', diceTypesToRoll);
-  if (threeJSManager) {
-    threeJSManager.rollDice(diceTypesToRoll).then(results => {
-      results.forEach(result => console.log(`${result.type}면체 주사위 결과: ${result.value}`));
-    });
-  }
-};
-
-onMounted(() => {
-  threeJSManager = new ThreeJSManager(rendererContainer.value);
-  eventBus.on('roll-dice', handleRollDice);
-});
-
-onUnmounted(() => {
-  eventBus.off('roll-dice', handleRollDice);
 
 const tokenImage = require('@/assets/images/ingame/Token.png');
 const mapImage = require('@/assets/images/maps/map1.png');
@@ -138,28 +109,14 @@ onUnmounted(() => {
 .map-section-container {
   width: 100%;
   height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  overflow: hidden;
   position: relative;
+  overflow: hidden;
 }
 
 .map-image {
   width: 100%;
   height: 100%;
   object-fit: cover;
-  z-index: 1;
-}
-
-.renderer-container {
-  width: 100%;
-  height: 80vh;
-  background-color: transparent; /* Change this to transparent */
-  position: absolute; /* Make it position absolute */
-  top: 0;
-  left: 0;
-  z-index: 2; /* Set z-index to be above the map image */
 }
 
 .token {

9b4e5487d9470f61ff62af2b7b331dd89c542ace
정수현 <jshinkor00@naver.com>
2024-07-29 23:17:54 +0900

    Chore: remove comment

--- frontEnd/src/views/main/components/SocialLoginModal.vue
+++ frontEnd/src/views/main/components/SocialLoginModal.vue
@@ -29,8 +29,6 @@ const googleLogin = () => {
 
   const url = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=${responseType}&scope=${scope}`;
 
-  // const url = 'https://accounts.google.com/o/oauth2/v2/auth?client_id=288654374576-oakoiq7biqtduolbs3rfgef4cb30umr1.apps.googleusercontent.com&redirect_uri=https://i11d108.p.ssafy.io/api/v1/members/auth/login/google&response_type=code&scope=email profile';
-
   window.location.href = url;
   console.log("url = "+ url);
 };

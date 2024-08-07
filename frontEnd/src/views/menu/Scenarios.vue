2eb95dec7bcf00bef24b96501c40775e8f8018f0
jshinkor00 <jshinkor00@naver.com>
2024-08-08 04:17:53 +0900

    Feat-FE: scenario list page (#68)
    시나리오 목록 조회 페이지 컨테이너 생성

--- frontEnd/src/views/menu/Scenarios.vue
+++ frontEnd/src/views/menu/Scenarios.vue
@@ -1,17 +1,38 @@
 <template>
-    <div>
-      <h1>시나리오 페이지</h1>
-      <p>dd</p>
+  <div class="background">
+    <div class="container">
+      <h1 class="menu-title">시나리오</h1>
+      <div style="background-color: gray; height: 90vh">
+        <ScenarioCard />
+      </div>
     </div>
-  </template>
-  
-  <script>
-  export default {
-    name: 'Scenarios'
-  };
-  </script>
-  
-  <style scoped>
-  /* 시나리오 페이지 스타일 */
-  </style>
-  
+  </div>
+</template>
+
+<script>
+import component from "element-plus/es/components/tree-select/src/tree-select-option.mjs";
+import ScenarioCard from "./components/ScenarioCard.vue";
+
+export default {
+  name: "Scenarios",
+  components: {
+    ScenarioCard,
+  },
+};
+</script>
+
+<style scoped>
+.background {
+  width: 100%;
+  height: 100vh;
+  background: radial-gradient(circle, #262626 0%, #232428 100%);
+}
+.container {
+  max-width: 1200px;
+  padding-top: 80px;
+}
+.menu-title {
+  color: white;
+  margin: 50px 0;
+}
+</style>
---
+++ frontEnd/src/views/menu/components/ScenarioCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="card-background">
+    <div class="image-container">
+      <img class="card-image" src="@/assets/images/popular/card1.jpg" alt="" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ScenarioCard",
+};
+</script>
+
+<style scoped>
+.card-background {
+  background-color: #3c3d41;
+  width: 300px;
+  height: 400px;
+  border-radius: 1ch;
+  padding: 20px;
+}
+.image-container {
+  height: 270px;
+}
+.card-image {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  border-radius: 1ch;
+}
+</style>

9c781a978e8abcd9e5940d1ca9a594a96afe04f3
namgyeongmin <skarudals27@naver.com>
2024-08-08 23:59:36 +0900

    Rebasing 29-feat-be-websocket-chat-api

--- backEnd/SIXTALEBackEnd/src/main/java/org/infinity/sixtalebackend/global/config/WebConfig.java
+++ backEnd/SIXTALEBackEnd/src/main/java/org/infinity/sixtalebackend/global/config/WebConfig.java
@@ -3,8 +3,8 @@ package org.infinity.sixtalebackend.global.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebMvc

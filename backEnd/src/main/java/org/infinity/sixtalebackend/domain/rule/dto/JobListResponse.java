7529ca0f0c374b62adbc74a864e8c5230df70287
jshinkor00 <jshinkor00@naver.com>
2024-08-10 00:29:34 +0900

    Fix-BE: fix job list of rule API
    룰 직업 목록 조회 API 수정

--- backEnd/src/main/java/org/infinity/sixtalebackend/domain/rule/dto/JobListResponse.java
+++ backEnd/src/main/java/org/infinity/sixtalebackend/domain/rule/dto/JobListResponse.java
@@ -1,5 +1,6 @@
 package org.infinity.sixtalebackend.domain.rule.dto;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import org.infinity.sixtalebackend.domain.rule.domain.Job;
@@ -7,7 +8,7 @@ import org.infinity.sixtalebackend.domain.rule.domain.Job;
 import java.util.List;
 
 @Getter
-@Setter
+@Builder
 public class JobListResponse {
-    private List<Job> jobList;
+    private List<JobResponse> jobList;
 }

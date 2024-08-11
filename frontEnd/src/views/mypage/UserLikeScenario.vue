9001c6e49a7aca8bbd2cbc39e0db1468be3792d9
jshinkor00 <jshinkor00@naver.com>
2024-08-12 02:38:22 +0900

    Feat-FE: MyPage - Liked Scenarios List
    마이페이지 - 좋아요한 시나리오 목록 조회 구현

--- frontEnd/src/views/mypage/UserLikeScenario.vue
+++ frontEnd/src/views/mypage/UserLikeScenario.vue
@@ -1,20 +1,40 @@
 <template>
   <div><h3 class="menu-title">좋아요한 시나리오</h3></div>
   <div class="content-box right-box">
-    <!-- <ScenarioCard /> -->
+    <div class="card-container">
+      <div class="card-inner-container">
+        <ScenarioCard
+          class="card-item"
+          v-for="scenario in likedScenarios.scenarioList"
+          :key="scenario.id"
+          :scenario="scenario"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import { ref, onMounted } from "vue";
 import ScenarioCard from "../menu/components/ScenarioCard.vue";
-
-onMounted();
+import { getLikedScenarioList } from "../../common/api/mypageAPI";
 
 export default {
   name: "UserLikeScenario",
   components: {
     ScenarioCard,
+  },
+  setup() {
+    const likedScenarios = ref([]);
+
+    onMounted(async () => {
+      likedScenarios.value = await getLikedScenarioList();
+      console.log("liked", likedScenarios.value.scenarioList);
+    });
+
+    return {
+      likedScenarios,
+    };
   },
 };
 </script>
@@ -25,8 +45,8 @@ export default {
   margin: 0 0 20px 20px;
 }
 .content-box {
-  background-color: #3a3a3c;
-  border-radius: 18px;
+  /* background-color: #3a3a3c; */
+  /* border-radius: 18px; */
 }
 .left-box {
   height: 750x;
@@ -35,7 +55,18 @@ export default {
 }
 .right-box {
   height: 695px; /*왼쪽 박스와 높이 끝 같게 */
-  padding: 20px;
-  border: 1px solid #3a3a3c;
+  /* border: 1px solid #3a3a3c; */
+}
+.card-container {
+  padding-left: 20px;
+  display: flex;
+  justify-content: center;
+}
+.card-inner-container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: start;
+  gap: 20px;
+  width: 100%;
 }
 </style>

2dd83d3c47a72b323b681b654be31f02379eb175
anchisook <songweddin@naver.com>
2024-08-12 11:32:02 +0900

    Fix-BE: update application.properties

--- frontEnd/vue.config.js
+++ frontEnd/vue.config.js
@@ -33,11 +33,11 @@ module.exports = {
     open: true,
     proxy: {
       '/api/v1': {
-        target: 'http://i11d108.p.ssafy.io:8888/', // 백엔드 서버 주소로 변경
+        target: 'http://localhost:8888/', // 백엔드 서버 주소로 변경
         changeOrigin: true
       },
       '/ws': {
-        target: 'http://i11d108.p.ssafy.io:8888/api/v1', // WebSocket 서버 주소
+        target: 'http://localhost:8888/api/v1', // WebSocket 서버 주소
         ws: true, // WebSocket 프록시 활성화
         changeOrigin: true,
       },
